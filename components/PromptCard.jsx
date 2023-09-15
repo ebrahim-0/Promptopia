775fbec533436fbd36283ca3f03fa41b0a47538e
ebrahin-0 <ebrahimabdelrazik2002@gmail.com>
2023-09-15 18:57:52 +0300

    Fixed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -14,8 +14,6 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
   const handleProfileClick = () => {
-    console.log(post.creator._id);
-    console.log(session?.user?.id);
     if (post.creator._id === session?.user?.id) return router.push("/profile");
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
