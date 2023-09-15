a7c6d2f9aed4c160ed4d21283720244c08461a78
ebrahin-0 <ebrahimabdelrazik2002@gmail.com>
2023-09-15 18:56:45 +0300

    Fixed

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-  mongoose.set("strictQuery", true);
+  // mongoose.set("strictQuery", true);
+  mongoose.set("strict", true);
 
   if (isConnected) {
     console.log("MongoDB is already connected");
